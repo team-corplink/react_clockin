6d6152b15da0f84e40b79a5d89b31315f6e88ee8
Glenn <57659894+dev-glennn@users.noreply.github.com>
2020-06-11 18:50:48 +0900

    style: padding

--- src/components/common/Template.js
+++ src/components/common/Template.js
@@ -21,15 +21,23 @@ export default Template;
 const sizes = {
     main: {
         width: '250px',
+        padding: '24px',
     },
     member: {
         width: '50%',
+        padding: '0px',
     }
 }
 
 const sizeStyles = css`
     ${({type}) => css`
         width: ${sizes[type].width};
+        `}
+`;
+
+const paddingStyles = css`
+    ${({type}) => css`
+        padding: ${sizes[type].padding};
         `}
 `;
 
@@ -56,5 +64,6 @@ const StyledTemplate = styled.div`
         display: flex;
         flex-direction: column;
         justify-content: flex-start;
+        ${paddingStyles}
     }
 `;
